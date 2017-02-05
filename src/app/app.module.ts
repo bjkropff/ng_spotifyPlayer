df8f6eedb7e9db28e8ec00b15b6bf46e9bb82d60
Brian Kropff <brian.kropff@MacBookAir-BrianKropff.domain>
2017-02-05 12:13:16 -0800

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }        from '@angular/core';
+import { BrowserModule }   from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }    from './app.component';
+
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent }  from './components/about/about.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+      AppComponent,
+      SearchComponent,
+      AboutComponent,
+      NavbarComponent,
+     ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
